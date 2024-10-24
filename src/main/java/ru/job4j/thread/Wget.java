f7d356e4d2e0cd70d69a017e3a20e02ae9f37c16
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2024-10-24 17:08:15 +0300

    8. Скачивание файла с ограничением.

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,46 @@
+package ru.job4j.thread;
+import java.io.*;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                int time = speed - bytesRead;
+                if (time < 0) {
+                    time = speed + bytesRead;
+                }
+                Thread.sleep(time);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        validateArgs(args.length);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+    private static void validateArgs(int argsNum) {
+        if (argsNum != 2) {
+            throw new IllegalArgumentException("Вы должны предоставить 2 аргумента (имя файла и ограничение скорости)");
+        }
+    }
+}
