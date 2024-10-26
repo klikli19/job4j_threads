b298985e679d8d1a2d470d3795072286d3608c8a
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2024-10-26 18:26:46 +0300

    8. Скачивание файла с ограничением. Правки

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,43 +5,60 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+            long downloadedBytes = 0;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                downloadedBytes += bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                int time = speed - bytesRead;
-                if (time < 0) {
-                    time = speed + bytesRead;
+                if (downloadedBytes >= speed) {
+                    long downloadAt = System.currentTimeMillis();
+                    long downloadTime = start - downloadAt;
+                    if (downloadTime < 1000) {
+                        Thread.sleep(downloadedBytes/speed * 1000 - downloadTime);
+                    }
                 }
-                Thread.sleep(time);
             }
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        validateArgs(args.length);
+        validateArgs(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
 
-    private static void validateArgs(int argsNum) {
-        if (argsNum != 2) {
-            throw new IllegalArgumentException("Вы должны предоставить 2 аргумента (имя файла и ограничение скорости)");
+    private static void validateArgs(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Недостаточно параметров");
+        }
+        if (args[0].isBlank()) {
+            throw new IllegalArgumentException("Некорректный 1 параметр");
+        }
+        if (args[1].isBlank() || Integer.parseInt(args[1]) == 0) {
+            throw new IllegalArgumentException("Некорректный 2 параметр");
+        }
+        if (args[2].isBlank()) {
+            throw new IllegalArgumentException("Некорректный 3 параметр");
         }
     }
 }
