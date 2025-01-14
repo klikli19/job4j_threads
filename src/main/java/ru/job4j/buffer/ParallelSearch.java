8f8b94f6ddd2be66721abb254b998d199e590e76
klikli19 <an.kilikova@yandex.ru>
2025-01-14 15:56:40 +0300

    2. Обеспечить остановку потребителя - correct

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -1,43 +1,49 @@
 package ru.job4j.buffer;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import ru.job4j.SimpleBlockingQueue;
 
 public class ParallelSearch {
 
     public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        AtomicBoolean flag = new AtomicBoolean(Boolean.TRUE);
+
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(1);
         final Thread consumer = new Thread(
-                () -> {
-                    while (!Thread.currentThread().isInterrupted()) {
-                        try {
-                            System.out.println(queue.poll());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }
+        () -> {
+            while (flag.get()) {
+            try {
+                System.out.println(queue.poll());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("consumer end");
+        Thread.currentThread().interrupt();
+    }
         );
-        consumer.start();
-
-        new Thread(
-                () -> {
-                    for (int index = 0; index != 3; index++) {
-                        try {
-                            queue.offer(index);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+    final Thread producer = new Thread(
+            () -> {
+                for (int index = 0; index != 5; index++) {
+                    try {
+                        queue.offer(index);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
-
-        ).start();
+                flag.set(Boolean.FALSE);
+                System.out.println("End");
+                Thread.currentThread().interrupt();
+            }
+    );
+        consumer.start();
+        producer.start();
         consumer.join();
-        consumer.interrupt();
+        System.out.println("Stop");
     }
 }
